a015d7f6a347123514f62a20c329395eb5b1e76e
alidrisy <a.alidrisy6@gmail.com>
2024-01-26 01:01:43 +0300

    1. Simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """ Model for index_range function """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple:
     """ Return a tuple of size two containing a start index
     and an end index of page """
