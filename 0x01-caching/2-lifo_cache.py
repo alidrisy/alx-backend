e46c0b881e62c384c6a97a38de7953eb621c7835
alidrisy <a.alidrisy6@gmail.com>
2024-01-31 19:20:48 +0300

    clean up fifo and lifo classes

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -19,13 +19,14 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key and item:
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                k = self.keys.pop()
-                print("DISCARD:", k)
-                self.cache_data.pop(k)
-            self.keys.append(key)
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            k = self.keys.pop()
+            print("DISCARD:", k)
+            self.cache_data.pop(k)
+        self.keys.append(key)
 
     def get(self, key):
         """ Get an item by key
