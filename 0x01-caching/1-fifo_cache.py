08d523790299bb8cac079c4954fbbd384645881d
alidrisy <a.alidrisy6@gmail.com>
2024-01-31 18:37:28 +0300

    1. FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-""" BaseCache module
+""" FIFOCache module
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache defines:
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines:
       - inherts from BaseCaching
       - constants of your cache system
       - where your data are stored (in a dictionary)
     """
+
+    def __init__(self):
+        """ Initiliaze """
+        super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
             self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                k = list(self.cache_data.keys())[0]
+                print("DISCARD: ",k)
+                self.cache_data.pop(k)
 
     def get(self, key):
         """ Get an item by key
