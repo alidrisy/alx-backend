f3675892d827556b027a36842422a1d180a7236e
alidrisy <a.alidrisy6@gmail.com>
2024-01-31 18:06:25 +0300

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,7 +14,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
