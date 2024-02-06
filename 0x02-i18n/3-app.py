be043c8f95ac2b7c2f1cff95cd4ac927de0b4369
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 22:17:26 +0300

    4. Force locale with URL parameter

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,6 +22,7 @@ def get_locale():
     """determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 # babel = Babel(app, locale_selector=get_locale)
 @app.route('/', strict_slashes=False)
 def index() -> str:
