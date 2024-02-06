bbbe882dc59eeb4590f99ac284e883fbbfba912c
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 22:18:25 +0300

    fix error

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,7 +21,7 @@ babel = Babel(app)
 def get_locale():
     """determine the best match with our supported languages."""
     locale = request.args.get('locale')
-    if local in Config.LANGUAGES:
+    if locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
