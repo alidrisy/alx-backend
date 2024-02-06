cd02a2f404d2b58269a8c3561e757fb60d7c8eaa
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 20:34:39 +0300

    1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" A basic Flask app """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ an object to configure the application """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """main route for our app"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
