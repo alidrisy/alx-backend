07f670097b827a8bfdb87f569aeceb11117eb56d
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 20:07:18 +0300

    0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" A basic Flask app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """main route"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
