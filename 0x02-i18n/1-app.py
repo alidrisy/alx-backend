662dba9588cdf6358658f968b02a1fd466863a11
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 20:44:08 +0300

    1. Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 class Config:
-    """ an object to configure the application """
+    """ an object to configure the new application """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
