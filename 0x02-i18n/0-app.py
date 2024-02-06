acc3e9098dd24bbaec1ca757072109633d8a47e8
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 20:23:39 +0300

    0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """main route for our app"""
-    return render_template("index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
