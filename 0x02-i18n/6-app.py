4dbb3547d96208de74c7c4863e18b27186f1257a
alidrisy <a.alidrisy6@gmail.com>
2024-02-06 23:20:41 +0300

    6. Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -53,7 +53,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """main route for our app"""
-    return render_template("5-index.html")
+    return render_template("6-index.html")
 
 
 if __name__ == "__main__":
