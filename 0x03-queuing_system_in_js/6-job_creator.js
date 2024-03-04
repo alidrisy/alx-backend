11b14fcf2b1ead79e3aeecb5eaad7c082f7e00ec
alidrisy <a.alidrisy6@gmail.com>
2024-03-04 23:28:17 +0300

    9. Track progress and errors with Kue: Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,15 +3,20 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: 'string',
-  message: 'string',
-}).save((err) => {
-  if (err) {
-    console.log('Notification job failed');
-  } else {
-    console.log(`Notification job created: ${job.id}`);
-  }
+  phoneNumber: '770727868',
+  message: 'Hello Holberton',
+})
+
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+})
+.on('enqueue', () => {
+  console.log('Notification job created:', job.id);
+})
+.on('failed', () => {
+  console.log('Notification job failed');
 });
-job.on('complete', (result) => {
-  console.log('Notification job completed')
-});
+  
+job.save();
+  
