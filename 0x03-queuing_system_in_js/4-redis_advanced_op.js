c6ea9a2c6981dbf15b90a70861491e2e7fffc553
alidrisy <a.alidrisy6@gmail.com>
2024-03-04 21:59:54 +0300

    4. Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,29 @@
+import { promisify } from 'util';
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+const hash = {
+  'Portland': 50,
+  'Seattle': 80,
+  'New York': 20,
+  'Bogota': 20,
+  'Cali': 40,
+  'Paris': 2,
+};
+
+for (const [key, val] of Object.entries(hash)) {
+  client.hset('HolbertonSchools', key, val, redis.print);
+}
+
+client.hgetall('HolbertonSchools', (err, reply) => {
+  console.log(reply);
+});
